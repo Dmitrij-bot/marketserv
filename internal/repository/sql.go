3fbe3a4bbe0377a607bc60b0b34f2efed7d1d649
Дмитрий <Dmitrijdrannikov@gmail.com>
2024-10-10 19:56:29 +0300

    1

--- internal/repository/sql.go
+++ internal/repository/sql.go
@@ -2,5 +2,5 @@ package repository
 
 const (
 	FindClientByUserNameSql = "SELECT id,username,role FROM clients_table WHERE id = $1"
-	SearchProductByNameSQL  = "SELECT id,name,description,price FROM products WHERE name =1$"
+	SearchProductByNameSQL  = "SELECT id, name, description, price FROM products WHERE name ILIKE '%' || $1 || '%'"
 )
